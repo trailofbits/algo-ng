e473a95a8c1c756b3f567a803710087e7983ee97
Jack Ivanov <e601809@gmail.com>
2017-09-29 16:27:49 +0200

    disable reboot, add gitignore

--- cloud-init/main.sh
+++ cloud-init/main.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 set -x
 
+cat << 'EOF' > /root/inventory
+[local]
+localhost ansible_connection=local
+EOF
+
 sudo apt-get update
 sudo apt-get install software-properties-common -y
 sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update
 sudo apt-get install ansible -y
-ansible-pull -U https://github.com/trailofbits/algo-ng -e 'server_name=${server_name} vpn_users=${vpn_users}'
+ansible-pull -U https://github.com/trailofbits/algo-ng -e 'server_name=${server_name} vpn_users=${vpn_users}' -i /root/inventory
