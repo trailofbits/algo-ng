f9559fa9c73b419c5aea862d525f3bad59e5c428
Luis Lavena <luislavena@gmail.com>
2018-02-07 16:27:16 -0300

    Ensure Digital Ocean tag exist and use it
    Digital Ocean's provider indicates that `tags` references must exist
    prior it's usage.
    
    Attempt to provision of a non-existing tag results in 404 error, causing
    the provisioning process to abort.
    
    This change ensures the tag is created (or noop if already exists) and
    associates it correctly to the instance.

--- modules/cloud-digitalocean/resources.tf
+++ modules/cloud-digitalocean/resources.tf
@@ -3,13 +3,17 @@ resource "digitalocean_ssh_key" "algo" {
   public_key = "${var.public_key_openssh}"
 }
 
+resource "digitalocean_tag" "algo" {
+  name = "Environment:Algo"
+}
+
 resource "digitalocean_droplet" "algo" {
   name      = "${var.algo_name}"
   image     = "${var.image}"
   size      = "${var.size}"
   region    = "${var.region}"
   user_data = "${var.user_data}"
-  tags      = [ "Environment:Algo" ]
+  tags      = [ "${digitalocean_tag.algo.id}" ]
   ssh_keys  = [ "${digitalocean_ssh_key.algo.id}" ]
   ipv6      = true
 }
